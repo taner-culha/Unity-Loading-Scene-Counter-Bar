1d1679e8f9bdb5ff2864baa7fb7f1697f7e80dd1
taner-culha <tanerculha@outlook.com.tr>
2022-01-18 23:29:00 +0300

    Unity Loading Scene Counter Bar
    Unity progress bar and timer loading scene

---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Loading : MonoBehaviour
+{
+    public float loading;
+    public Text loadingBar;
+    public GameObject bar;
+    void Update()
+    {
+        loadingBar.text = "%" + (int)loading;
+        bar.transform.localScale = new Vector2(loading / 100, 1);
+        if (loading < 100)
+        {
+            loading += Time.deltaTime * 50;
+        }
+        if (loading >= 100)
+        {
+            loading = 100;
+            SceneManager.LoadScene("Menu");
+        } 
+    }
+}
